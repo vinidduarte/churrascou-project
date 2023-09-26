b073f8f767d7b6052abc32856295e09f8dc8b028
Vinicius Drumond Duarte <viniciusdduarte@outlook.com>
2023-09-26 15:33:06 -0300

    Tentando corrigir problemas

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
-
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App.js';
+import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
